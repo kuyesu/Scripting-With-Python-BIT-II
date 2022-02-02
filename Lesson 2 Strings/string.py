d676ddaa649a90ec42d326e5b35ea70ddd142c47
kuyesu <2019bce028@std.must.ac.ug>
2022-02-02 15:13:49 +0300

    lesson

---
+++ Lesson 2 Strings/string.py
@@ -0,0 +1,36 @@
+""" String dataType"""
+
+name = "Joan"
+likes = "Chips"
+year = "year two"
+age = 15
+
+"Joan is 15 and she is in year two. She likes chips"
+
+""" String concatination"""
+""" There are three ways of concatinating strings"""
+
+""" 
+- +
+- f function
+- format function
+"""
+
+name = "Joan"
+likes = "Chips"
+year = "year two"
+age = 15
+
+#"Joan is 15 and she is in year two. She likes chips"
+print(name + " " + "is " + str(age) + " she is in " + year + "She likes " + likes)
+
+
+"Joan is 15 and she is in year two. She likes chips"
+# print("{} is {} and she is in {}. She likes {}".format(name, age, likes))
+
+""" getting input from the user"""
+
+name = input("Enetr your name: ")
+age = str(input("Enter your age: "))
+likes = input("Enter what you like: ")
+
---
+++ Lesson 3 DataTypes/README.md
@@ -0,0 +1,30 @@
+### Python DataTypes
+
+## types of DataTypes
+ - Text dataType
+    - Strings (str)
+
+- Numeric dataTypes
+    - integers
+    - floats
+    - complex numbers
+
+- Sequence dataTypes
+    - list 
+    - tuple
+    - range
+
+- Mapping datatypes
+    - dictionary (dict)
+
+- Set datatypes
+    - set
+    - frozenset
+
+- Boolean datatype
+    - bool (by default return True or False)
+
+- Binary datatypes
+    - bytes
+    - bytearray
+    - memoryview
