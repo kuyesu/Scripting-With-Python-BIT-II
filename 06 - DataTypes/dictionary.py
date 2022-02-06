87c5a90dab252e867ed4beb7ca46010fbee9b351
kuyesu <2019bce028@std.must.ac.ug>
2022-02-06 12:19:41 +0300

    handling conditions

---
+++ 06 - DataTypes/dictionary.py
@@ -0,0 +1,7 @@
+""" Dictionary"""
+
+student = {"name" : "Rogers", "age" : 23, "regno" : "2020/bit/042/ps"}
+
+
+
+"engaging students in technologies that solve the world pressing challenges around us"
--- Lesson 3 DataTypes/dictionary.py
+++
@@ -1,3 +0,0 @@
-""" Dictionary"""
-
-student = {"name" : "Rogers", "age" : 23, "regno" : "2020/bit/042/ps"}
