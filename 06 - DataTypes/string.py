30e1b67575462c86289d6471997dd280d0479b14
kuyesu <2019bce028@std.must.ac.ug>
2022-02-18 09:16:24 +0000

    update list

---
+++ 06 - DataTypes/string.py
@@ -0,0 +1,26 @@
+name = "Frank"
+age = "24"
+status = "single"
+print(name, age, status)
+print(name+str(age)+status)
+print(f"my name is {name} and my age is {age}")
+print("my name is {} and my age is {}".format(name, age))
+'''''LISTS'''
+items = [1, 2, 3, 4, 5]
+
+print(items[0])
+print(items[::-1])
+''''list complehension'''
+numbers = [i for i in range(100,201)]
+print(numbers)
+names = ["Bruno", "Faizo", "Daniel","Jesus","Jehovah"]
+for name in names:
+    print(names[0])
+    break
+
+
+"GAME"
+Questions = [
+    "1. "
+
+]
